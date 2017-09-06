389e1934219e83d59c9756433e3ae5841b29d292
tberti81 <tberti@freemail.hu>
2017-09-07 00:54:41 +0200

    added sudoku validator with tests

---
+++ src/SudokuValidator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bertold
+ * Date: 2017.09.06.
+ * Time: 21:22
+ */
+
+namespace Sudoku;
+
+
+class SudokuValidator {
+
+} 
---
+++ test/SudokuValidatorTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bertold
+ * Date: 2017.09.06.
+ * Time: 21:14
+ */
+
+namespace Sudoku\Test;
+
+
+class SudokuValidatorTest {
+
+} 
